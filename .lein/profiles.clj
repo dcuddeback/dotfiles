e77ef26f68bff5503a8531ff975a0c90c44b1dba
David Cuddeback <david.cuddeback@gmail.com>
2013-03-20 10:20:19 -0700

    adds lein-midje plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-pedantic "0.0.5"]
                   [lein-pprint "1.1.1"]
+                  [lein-midje "3.0.0"]
                   [lein-tarsier "0.10.0"]]
         :vimclojure-opts {:repl true}}}
