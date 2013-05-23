a2e0f67637054fd00c1699ecdef9b012a28a13ee
David Cuddeback <david.cuddeback@gmail.com>
2013-05-23 16:37:48 -0700

    adds lein-release plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[lein-pedantic "0.0.5"]
                   [lein-pprint "1.1.1"]
                   [lein-midje "3.0.0"]
+                  [lein-release "1.0.4"]
                   [lein-tarsier "0.10.0"]]
         :vimclojure-opts {:repl true}}}
