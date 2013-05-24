c893aabc0d9e53ee5100269708d13397b3a2f202
David Cuddeback <david.cuddeback@gmail.com>
2013-05-23 17:53:16 -0700

    installs lein-autoreload plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,5 +2,6 @@
                   [lein-pprint "1.1.1"]
                   [lein-midje "3.0.0"]
                   [lein-release "1.0.4"]
+                  [lein-autoreload "0.1.0"]
                   [lein-tarsier "0.10.0"]]
         :vimclojure-opts {:repl true}}}
