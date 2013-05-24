841aeb96196dfd3fbc267e6928f6fc4ef7efd5c1
David Cuddeback <david.cuddeback@gmail.com>
2013-05-23 17:54:05 -0700

    installs lein-vanity plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,6 +2,7 @@
                   [lein-pprint "1.1.1"]
                   [lein-midje "3.0.0"]
                   [lein-release "1.0.4"]
+                  [lein-vanity "0.1.0"]
                   [lein-autoreload "0.1.0"]
                   [lein-tarsier "0.10.0"]]
         :vimclojure-opts {:repl true}}}
