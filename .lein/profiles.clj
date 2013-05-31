ff9cb7562936e90e879e1a75b25470eaf4f15371
David Cuddeback <david.cuddeback@gmail.com>
2013-05-30 21:42:02 -0700

    installs lein-marginalia plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,7 @@
 {:user {:plugins [[lein-pedantic "0.0.5"]
                   [lein-pprint "1.1.1"]
                   [lein-midje "3.0.0"]
+                  [lein-marginalia "0.7.1"]
                   [lein-release "1.0.4"]
                   [lein-vanity "0.1.0"]
                   [lein-autoreload "0.1.0"]
