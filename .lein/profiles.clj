f9b351172a0d9888eee9c771aef09fa4c414cbeb
David Cuddeback <david.cuddeback@gmail.com>
2015-03-29 13:52:53 -0700

    removes lein-pedantic plugin

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,11 +1,10 @@
-{:user {:plugins [[lein-pedantic "0.0.5"]
-                  [lein-pprint "1.1.1"]
-                  [lein-midje "3.0.0"]
-                  [lein-marginalia "0.7.1"]
-                  [lein-release "1.0.4"]
-                  [lein-vanity "0.1.0"]
-                  [lein-autoreload "0.1.0"]
-                  [lein-tarsier "0.10.0"]]
+{:user { :plugins [[lein-pprint "1.1.1"]
+                   [lein-midje "3.0.0"]
+                   [lein-marginalia "0.7.1"]
+                   [lein-release "1.0.4"]
+                   [lein-vanity "0.1.0"]
+                   [lein-autoreload "0.1.0"]
+                   [lein-tarsier "0.10.0"]]
         :dependencies [[midje-progress-emitter "0.1.0"]]
         :vimclojure-opts {:repl true}}
  :lint {:global-vars {*warn-on-reflection* true}}}
