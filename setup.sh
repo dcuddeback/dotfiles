bd912fdbb770b387526f4629b73a615eda1ad222
David Cuddeback <david.cuddeback@gmail.com>
2016-08-30 21:12:31 -0700

    fix shared ~/.cargo directory

--- setup.sh
+++ setup.sh
@@ -5,13 +5,21 @@ if [ -f "$HOME/.bashrc" -a \! -L "$HOME/.bashrc" ]; then
   mv -v "$HOME/.bashrc" "$HOME/.bashrc_default"
 fi
 
-# link dotfiles (skip .git directory and setup.sh)
+# link dotfiles (skip .git directory and setup.sh; handle .cargo/config separately)
 for file in $(ls -A "$HOME/dotfiles/"); do
-  if [ "$file" = ".git" -o "$file" = "setup.sh" ]; then
+  if [ "$file" = ".git" -o "$file" = "setup.sh" -o "$file" = ".cargo" ]; then
     continue
   fi
 
-  if [ "$file" != ".git" -a \! -e "$HOME/$file" ]; then
+  if [ \! -e "$HOME/$file" ]; then
     ln -sv "$HOME/dotfiles/$file" $HOME
   fi
 done
+
+if [ \! -L "$HOME/.cargo/config" ]; then
+  if [ \! -d "$HOME/.cargo" ]; then
+    mkdir "$HOME/.cargo"
+  fi
+
+  ln -sv "$HOME/.cargo/config" "$HOME/.cargo/"
+fi
